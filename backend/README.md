f11311db1172a5973bb406da6dc07185da40715d
Danilo Marques <danilomarques20@hotmail.com>
2020-01-12 13:12:06 -0300

    Adding eslint

--- backend/README.md
+++ backend/README.md
@@ -1,8 +1,11 @@
 # Backend Pomodoro
 
 ## TODO:
-* Registro do usuario
-    * Model do usuario
-    * Controller para criar o usuario
-* Autenticacao do usuario
-    * Criar um token para o usuario
+
+- Configurar eslint
+- Configurar variaveis de ambiente
+- Registro do usuario
+  - Model do usuario
+  - Controller para criar o usuario
+- Autenticacao do usuario
+  - Criar um token para o usuario
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,20 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
+import routes from './routes';
 
 class App {
-    private express: Application;
-    constructor() {
-        this.express = express();
-    }
-    
-    public bootstrap(PORT: number): void {
-        this.express.listen(PORT);
-    }
+  private express: Application;
+
+  constructor() {
+    this.express = express();
+  }
+
+  public bootstrap(PORT: number): void {
+    this.express.listen(PORT);
+  }
+
+  public middlewares(): void {
+    this.express.use(routes);
+  }
 }
 
 export default new App();
