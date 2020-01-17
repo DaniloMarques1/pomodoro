51313f30d3185c03d05b458bf8dfae4bf0dfcac4
Danilo Marques <danilomarques20@hotmail.com>
2020-01-16 22:14:38 -0300

    Removendo console.log

--- backend/src/controllers/SessionController.ts
+++ backend/src/controllers/SessionController.ts
@@ -7,13 +7,11 @@ import Utils from '../utils/util';
 export default abstract class SessionController {
   static async store(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
-    console.log(password);
     const user = await UserModel.findOne({ email });
     if (!user) {
       return res.status(401).json({ error: 'Email incorrect' });
     }
     const match = await bcrypt.compare(password, user.password);
-    // console.log(match);
     if (match) {
       const payload: UserPayload = {
         id: user._id,
